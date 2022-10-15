8004b0193a1de5bfc41babea92a8fe4a36cfd26e
kuha1088 <kuha1088@mail.ru>
2022-10-15 13:43:56 +0300

    Added Ex011

---
+++ Ex011/Program.cs
@@ -0,0 +1,50 @@
+﻿void FillArray(int[] Collection)
+{
+   int lenght = Collection.Length;
+   int Index = 0;
+   while (Index < lenght)
+   {
+       Collection[Index] = new Random().Next(1, 10);
+       Index++;
+   }
+}
+
+void PrintArray(int[] Col)
+{
+   int Count = Col.Length;
+   int Position = 0;
+   while (Position < Count)
+   {
+        Console.WriteLine(Col[Position]);
+        Position++;
+   }
+}
+
+
+int IndexOf(int[] Collection, int Find)
+{
+    int Count = Collection.Length;
+    int Index = 0;
+    int Position = -1;
+    while(Index < Count)
+    {
+        if(Collection[Index] == Find)
+        {
+          Position = Index;
+          break;
+        }
+        Index++;
+    }
+    return Position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+array[4] = 4;
+array[6] = 6;
+PrintArray(array);
+Console.WriteLine();
+
+int Pos = IndexOf(array, 4444);
+Console.WriteLine(Pos);
