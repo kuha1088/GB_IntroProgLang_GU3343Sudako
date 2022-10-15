9b1f5a36db194c7d9adf03c220d441513e4fb71b
kuha1088 <kuha1088@mail.ru>
2022-10-15 11:06:29 +0300

    Added Ex1-004

---
+++ Ex1-004/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
+// 2 -> " -2, -1, 0, 1, 2"
+
+Console.Write("Введите число: ");
+string UserNumber = Console.ReadLine();
+int Number= int.Parse(UserNumber);
+
+for (int i = -Number; i < Number; i++)
+{
+    Console.Write($"{i}, ");
+}
+
