c20bd0e3bada36d3f23d30980d71eebdd0d351f3
kuha1088 <kuha1088@mail.ru>
2022-10-15 12:59:52 +0300

    Added

---
+++ Ex010/Program.cs
@@ -0,0 +1,19 @@
+﻿int [] array = {1, 12, 31, 4, 18, 15, 16, 17, 18};
+
+int n = array.Length;
+int Find = 18;
+
+int Index = 0;
+
+while (Index < n)
+{
+    if(array[Index] == Find)
+    {
+        Console.WriteLine(Index);
+        break;
+    }
+    
+    //index = index = index + 1
+    Index++;
+}
+
