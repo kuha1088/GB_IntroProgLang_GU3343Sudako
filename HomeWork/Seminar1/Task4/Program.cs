8c4572bc2ffcba3ee9cf3ff72de292d63f673398
kuha1088 <kuha1088@mail.ru>
2022-10-15 19:55:44 +0300

    Added HomeWork Seminar1 Task4

---
+++ HomeWork/Seminar1/Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число: ");
+string UserNumber = Console.ReadLine();
+int Number = int.Parse(UserNumber);
+
+for (int i = 2; i <= Number; i = i + 2)
+{
+    Console.Write($"{i}, ");
+}
