86a7275160d92b6157210a54871a400bac0b5d4f
kuha1088 <kuha1088@mail.ru>
2022-10-15 11:30:34 +0300

    Added Ex1-005

---
+++ Ex1-005/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+// 	456 -> 6
+// 	782 -> 2
+// 	918 -> 8
+
+Console.WriteLine("Введите трехзначное число: ");
+string UserNumber = Console.ReadLine();
+int Number = int.Parse(UserNumber);
+
+
+if (Number >= 100 && Number <= 999 || Number <= -100 && Number >= -999)
+{
+    int Result = Math.Abs(Number % 10);
+Console.WriteLine($"Последняя цифра этого числа: {Result}");
+}
+else
+{
+Console.WriteLine($"Введите другое число, это число не трехзначное");
+}  
+
