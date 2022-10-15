688dedba471432a9711fc1e81544679311074b5d
kuha1088 <kuha1088@mail.ru>
2022-10-15 10:57:30 +0300

    Added Ex1-003

---
+++ Ex1-003/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 	3 -> Среда 
+// 5 -> Пятница
+
+Console.Write("Какой по счету сегодня день недели?: ");
+string UserNumber = Console.ReadLine();
+int DayNumber= int.Parse(UserNumber);
+if (DayNumber == 1) Console.Write("понедельник");
+if (DayNumber == 2) Console.Write("вторник");
+if (DayNumber == 3) Console.Write("среда");
+if (DayNumber == 5) Console.Write("пятница");
+if (DayNumber == 6) Console.Write("суббота");
+if (DayNumber == 7) Console.Write("воскресенье");
