ed74c7036c28c7128c0ad9eb85b9b908f279316a
kuha1088 <kuha1088@mail.ru>
2022-10-15 19:43:04 +0300

    Added HomeWork Seminar1 Task3

---
+++ HomeWork/Seminar1/Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Write("Введите число, которое хотите проверить: ");
+string UserNumber = Console.ReadLine();
+int Number = int.Parse(UserNumber);
+
+if(Number % 2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
