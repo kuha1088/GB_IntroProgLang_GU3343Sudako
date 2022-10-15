06f2a772fd1e2caa24793d3a34635c971bec53a7
kuha1088 <kuha1088@mail.ru>
2022-10-15 10:43:39 +0300

    Added Ex1-001, Ex1-002

---
+++ Ex1-001/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+
+// Например:
+// 4 -> 16 
+// -3 -> 9 
+// -7 -> 49
+
+Console.WriteLine("Введите число, которое хотите возвести в степень: ");
+string NumberStr = Console.ReadLine();
+//То что вводит пользователь с консоли это всегда тип "string"
+int NumberInt = int.Parse(NumberStr);
+int Result = NumberInt * NumberInt;
+Console.WriteLine($"Квадрат числа {NumberInt} равен {Result}");
---
+++ Ex1-002/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+// a = 25, b = 5 -> да 
+// a = 2, b = 10 -> нет 
+// a = 9, b = -3 -> да 
+// a = -3 b = 9 -> нет
+
+Console.WriteLine("Введите число 1, которое хотите проверить: ");
+string NumberStr1 = Console.ReadLine();
+Console.WriteLine("Введите число 2, которое хотите проверить: ");
+string NumberStr2 = Console.ReadLine();
+int NumberInt1 = int.Parse(NumberStr1);
+int NumberInt2 = int.Parse(NumberStr2);
+
+int Result = NumberInt1 % NumberInt2;
+if (Result == 0)
+{
+    Console.WriteLine($"Является");
+}
+else
+{
+    Console.WriteLine($"Не является");
+}
