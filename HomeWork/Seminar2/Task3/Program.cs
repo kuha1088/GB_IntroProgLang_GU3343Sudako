c5af5535c8073b0994606c6b952e97e542eb6251
kuha1088 <kuha1088@mail.ru>
2022-10-16 13:08:37 +0300

    Added HomeWork Seminar2 Task3

---
+++ HomeWork/Seminar2/Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+int Get(string message)
+{
+   Console.WriteLine(message);
+   string UserNumberStr = Console.ReadLine();
+   int UserNumber = Convert.ToInt32(UserNumberStr);
+   return UserNumber;
+}
+
+int Number = Get("Введите число: ");
+
+if(Number == 6 || Number == 7)
+{
+    Console.WriteLine("Да, сегодня выходной");
+}
+else
+{
+    Console.WriteLine("Нет, сегодня будний день");
+}
