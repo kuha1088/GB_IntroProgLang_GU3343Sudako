aed94f7cbc7b2b27a2f164e4defd17bfca1c627f
kuha1088 <kuha1088@mail.ru>
2022-11-12 15:08:25 +0300

    Added HomeWork Seminar5 Task1

---
+++ HomeWork/Seminar5/Task1/Program.cs
@@ -0,0 +1,61 @@
+﻿/*
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+
+int Get(string msg)
+{
+    Console.WriteLine(msg);
+    while(true)
+    {
+        if(int.TryParse(Console.ReadLine(), out var userNumber))
+        return userNumber;
+        else
+        Console.WriteLine("Введенное значение не удовлетворяет требованиям задачи\n");
+    }
+}
+
+int[] InitArray(int UserNumber)
+{
+    Random rnd = new Random();
+    int lenght = UserNumber;
+    int[] result = new int[lenght];
+    
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = rnd.Next(100, 1000);
+    }
+
+    return result;
+}
+
+int EvenSum(int[] col)
+{
+    int count = 0;
+    for (int i = 0; i < col.Length; i++)
+    {
+    if (col[i]%2 == 0)
+        {
+        count++;
+        }
+    }
+    return count;
+}
+
+void PrintArray(int[] col)
+{
+    Console.WriteLine(string.Join(", ", col));
+}
+
+void PrintResult(int r)
+{
+    Console.WriteLine($"Количество чётных чисел в массиве: {r}");
+}
+
+
+int ArrLenght = Get("Введите число элементов массива: ");
+int[] arr = InitArray(ArrLenght);
+InitArray(ArrLenght);
+PrintArray(arr);
+int result = EvenSum(arr);
+PrintResult(result);
