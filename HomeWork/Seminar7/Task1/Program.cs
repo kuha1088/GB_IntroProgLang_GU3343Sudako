595b50c816af8a8e0d7d82fa47d0a7d17ac23979
kuha1088 <kuha1088@mail.ru>
2022-11-19 15:48:44 +0300

    Added HomeWork Seminar7

---
+++ HomeWork/Seminar7/Task1/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+Задача 47.
+Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+
+int GetNumber(string msg)
+{
+    Console.Write(msg);
+    int userNumber = Convert.ToInt32(Console.ReadLine());;
+    return userNumber;
+}
+
+double[,] InitArray(int m, int n)
+{
+    double [,] tempArray = new double[m,n];
+    Random rnd = new Random();
+    for (int i = 0; i < tempArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < tempArray.GetLength(1); j++)
+        {
+            tempArray[i, j] = Math.Round(rnd.Next(0, 100) + rnd.NextDouble(), 2);
+        }
+    }
+
+    return tempArray;
+}
+
+void PrintArray(double [,] tempArray)
+{
+    for (int i = 0; i < tempArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < tempArray.GetLength(1); j++)
+        {
+            Console.Write($"{tempArray[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = GetNumber("Введите число строк: ");
+int n = GetNumber("Введите число столбцов: ");
+double[,] array = InitArray(m, n);
+PrintArray(array);
---
+++ HomeWork/Seminar7/Task3/Program.cs
@@ -0,0 +1,70 @@
+﻿/*
+Задача 52.
+Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+
+int GetNumber(string msg)
+{
+    Console.Write(msg);
+    int userNumber = Convert.ToInt32(Console.ReadLine());;
+    return userNumber;
+}
+
+int[,] InitArray(int m, int n)
+{
+    int [,] tempArray = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < tempArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < tempArray.GetLength(1); j++)
+        {
+            tempArray[i, j] = rnd.Next(0, 100);
+        }
+    }
+
+    return tempArray;
+}
+
+void PrintArray(int [,] tempArray)
+{
+    for (int i = 0; i < tempArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < tempArray.GetLength(1); j++)
+        {
+            Console.Write($"{tempArray[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SearchColumnArithmeticMean(int [,] tempArray)
+{
+    double arithmeticMean = new double();
+    double storage = new double();
+
+    for (int j = 0; j < tempArray.GetLength(1); j++)
+    {
+        for (int i = 0; i < tempArray.GetLength(0); i++)
+        {
+            storage += tempArray[i, j]; 
+        }
+        arithmeticMean = Math.Round(storage / tempArray.GetLength(0), 1);
+        Console.WriteLine($"Среднее арифмитическое столбца №{j + 1} = {arithmeticMean}");
+        storage = 0;
+    }
+}
+
+
+int arrayStringNumber = GetNumber("Введите число строк: ");
+int arrayColumnNumber = GetNumber("Введите число столбцов: ");
+int[,] array = InitArray(arrayStringNumber, arrayColumnNumber);
+Console.WriteLine();
+Console.WriteLine("Двумерный массив: ");
+PrintArray(array);
+Console.WriteLine();
+SearchColumnArithmeticMean(array);
