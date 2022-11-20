3bf65f3c3ab2b7e6ba28e8fbfaf62949515ef103
kuha1088 <kuha1088@mail.ru>
2022-11-20 09:53:20 +0300

    Added Seminar7

---
+++ Seminar7/Ex7-001/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+int[,] InitArray(int m,int n)
+{
+int[,] array = new int[m,n];
+for (int i = 0; i < m; i++)
+{
+for (int j = 0; j < n; j++)
+{
+array[i,j] = i + j;
+}
+}
+return array;
+}
+
+void PrintArray(int[,] array)
+{
+for (int i = 0; i < array.GetLength(0); i++)
+{
+for (int j = 0; j < array.GetLength(1); j++)
+{
+Console.Write($"{array[i,j]} ");
+}
+Console.WriteLine();
+}
+}
+Console.WriteLine("Введите размер m: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите размер n: ");
+int n = int.Parse(Console.ReadLine());
+int[,] array = InitArray(m,n);
+PrintArray(array);
---
+++ Seminar7/Ex7-002/Program.cs
@@ -0,0 +1,82 @@
+﻿/*
+Задача 49:
+Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+*/
+
+
+int GetConsole(string msg, int x)
+{
+int result;
+
+while (true)
+{
+void LogWrong(string err)
+{
+Console.WriteLine(err);
+}
+
+Console.Write(msg);
+
+string str = Console.ReadLine() ?? "";
+
+if (int.TryParse(str, out int number) && number > x)
+{
+result = number;
+break;
+}
+else
+{
+LogWrong($"Нужно число > {x}.\n");
+}
+}
+
+return result;
+}
+
+int[,] InitArray(int m, int n)
+{
+Random rnd = new Random();
+int b;
+int[,] arr = new int[m,n];
+for (int i = 0; i < m; i++)
+{
+for (int j = 0; j < n; j++)
+{
+b = rnd.Next(-9, 10);
+arr[i,j] = b;
+}
+}
+return arr;
+}
+
+void Change(int[,] arr, int m, int n)
+{
+for (int i = 1; i < arr.GetLength(0); i+=2)
+{
+for (int j = 1; j < arr.GetLength(1); j+=2)
+{
+arr[i,j] = Convert.ToInt32(Math.Pow(arr[i,j],2));
+}
+}
+}
+
+void Vivod(int[,] arr, int m, int n)
+{
+for (int i = 0; i < m; i++)
+{
+for (int j = 0; j < n; j++)
+{
+if(j!=n-1){
+Console.Write($"{arr[i,j]} ");
+}else{
+Console.Write($"{arr[i,j]}\n");
+}
+}
+}
+}
+
+int m = GetConsole("Введите m: ", 0); //10
+int n = GetConsole("Введите n: ", 0); //10
+int[,] arr = InitArray(m, n);
+Change(arr, m, n); //10
+Vivod(arr, m, n); //10
