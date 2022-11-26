bdbd7746ad9be178d733ee1af047bb8bd55da8a3
kuha1088 <kuha1088@mail.ru>
2022-11-26 13:02:03 +0300

    Added HomeWork Seminar9

---
+++ HomeWork/Seminar9/Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Задача 66:
+Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int GetNumber(string msg)
+{
+    Console.Write(msg);
+    int userNumber = Convert.ToInt32(Console.ReadLine());
+    return userNumber;
+}
+
+int NumbersSum(int count, int number, int total)
+{
+    if(count > number) return total;
+    else
+    {
+        total += count;
+        return NumbersSum(count + 1, number, total);
+    }
+    
+}
+
+int userNumberM = GetNumber("Введите число M: ");
+int userNumberN = GetNumber("Введите число N: ");
+Console.WriteLine();
+int total = NumbersSum(count: userNumberM, number: userNumberN, total: 0);
+Console.Write($"Сумма натуральных элементов в промежутке от {userNumberM} до {userNumberN} = {total}");
+
---
+++ HomeWork/Seminar9/Task3/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Задача 68:
+Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+*/
+
+int GetNumber(string msg)
+{
+    Console.Write(msg);
+    while(true)
+    {
+        if(int.TryParse(Console.ReadLine(), out var userNumber))
+        if(userNumber >= 0)
+        return userNumber;
+        else
+        Console.WriteLine("Введенное значение не удовлетворяет требованиям задачи\n");
+    }
+}
+
+int Akk(int m, int n)
+{
+    if(m == 0) return n + 1;
+    else if ((m > 0) && (n == 0)) return Akk(m - 1, 1);
+    else if ((m > 0) && (n > 0)) return Akk(m - 1, Akk(m, n - 1));
+    else return n + 1;
+}
+
+int userNumberM = GetNumber("Введите число M: ");
+int userNumberN = GetNumber("Введите число N: ");
+Console.WriteLine();
+int result = Akk(m: userNumberM, n: userNumberN);
+Console.Write($"Функция Аккермана для {userNumberM} и {userNumberN} = {result}");
